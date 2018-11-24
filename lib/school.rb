f5d37adc894eac948fbc5eacac37d1e5930929e5
John Mark Redding <johnmarkredding@gmail.com>
2018-11-24 00:16:48 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+  end
+  
+  def grade(grade_to_select)
+    @roster[grade_to_select]
+  end
+  
+  def sort()
+    @roster.each_value do |students|
+      students.sort!
+    end
+  end
+end
